8c4042b046af2fdca0771be02865e9119b274bd0
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-02-10 14:12:48 +0600

    Inline Style/Dynamic Style

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,20 @@
 import React, {Component} from 'react';
-import './App.css';
-import Exp from './Exm';
+import "./App.css";
 
 
 
 
 class App extends Component{
   
-  state={
-    name:''
-  }
   
-  
-  
-  inputHandler=(event)=>{
-    this.setState({
-      name:event.target.value
-    })
-  }
   
   render(){
+    let obj={
+      backgroundColor:'skyblue',
+    }
     return (
-      <div className="App">
-        <div className="container my-3">
-          <input onChange={this.inputHandler} value={this.state.name} type="text" placeholder="Enter your name"/>
-          <button className="btn btn-primary"
-            onClick={(event)=>console.log(event)}>
-            Click me!
-          </button>
-          {this.state.name ? <p>Hello,{this.state.name}</p>:''}
-        
-        </div>
-        <Exp name={this.state.name}/>
+      <div className='App'>
+        <h1 style={{obj}}>Hello Program</h1>
       </div>
     );
   }
--- src/components/Exm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Exp=(props)=>{
-    return <h1>i am a stateless component, Name:{props.name}</h1>
-  }
-
-export default Exp;
